d9d738ee514f4d28ab47b18be9ec146f79134d2f
Ignacio Aguado <nachoaguadoc@MacBook-Pro-de-Ignacio.local>
2014-12-23 17:12:48 +0100

    Minor config changes

--- routes/video_controller.js
+++ routes/video_controller.js
@@ -1,31 +1,27 @@
 var videosRegistry = require('./../mdb/videosRegistry.js');
 var fs = require('fs');
 var path = require('path');
+var config = require('./../mitubo_config.js');
 /* GET home page. */
 exports.getList = function(req, res) {
 
 
-	fs.readdir("public/videos", function (err, files) {
+	fs.readdir(config.mitubo.videosPath, function (err, files) {
 		// console.log(files, err);
 		var videos = {};
 		var index = 0;
 		files.forEach (function (f) {
 			console.log(f);
 			var id = path.basename(f);
-			var url = "http://localhost:8080/"+ id;
+			var url = "http://" + config.mitubo.URL + ":" config.mitubo.port + "/" +  id;
 			videos[id] = url;
 
 		});
 
 			res.json(videos);
 
-		// res.render('index', {files: videos});
 	});
-	// videosRegistry.getList(function(videos){
-	// 	console.log(videos);
-	// 	res.send({"PRUEBA": "hola"});
 
-	// })
 
 };
 
